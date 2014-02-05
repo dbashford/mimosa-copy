db6dfd8ce6245528c315de7aa599e362386bd8ae
david.bashford <dbashford@bericotechnologies.com>
2014-02-05 13:40:31 -0500

    fixes dbashford/mimosa#359, copying mp3 by default

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ var logger = require( 'logmimosa' );
 exports.defaults = function() {
   return {
     copy: {
-      extensions: ["js","css","png","jpg","jpeg","gif","html","eot","svg","ttf","woff","otf","yaml","kml","ico","htc","htm","json","txt","xml","xsd","map","md","mp4"],
+      extensions: ["js","css","png","jpg","jpeg","gif","html","eot","svg","ttf","woff","otf","yaml","kml","ico","htc","htm","json","txt","xml","xsd","map","md","mp4","mp3"],
       exclude: []
     }
   };
@@ -17,7 +17,7 @@ exports.placeholder = function() {
          "  # the extensions of files to copy from sourceDir to compiledDir. vendor js/css, images, etc.\n" +
          "  ###\n" +
          "  # copy:              # config settings for the coffeescript compiler module\n" +
-         "    # extensions: [\"js\",\"css\",\"png\",\"jpg\",\"jpeg\",\"gif\",\"html\",\"eot\",\"svg\",\"ttf\",\"woff\",\"otf\",\"yaml\",\"kml\",\"ico\",\"htc\",\"htm\",\"json\",\"txt\",\"xml\",\"xsd\",\"map\",\"md\",\"mp4\"]\n" +
+         "    # extensions: [\"js\",\"css\",\"png\",\"jpg\",\"jpeg\",\"gif\",\"html\",\"eot\",\"svg\",\"ttf\",\"woff\",\"otf\",\"yaml\",\"kml\",\"ico\",\"htc\",\"htm\",\"json\",\"txt\",\"xml\",\"xsd\",\"map\",\"md\",\"mp4\",\"mp3\"]\n" +
          "    # exclude: []      # List of regexes or strings to match files that should not be copied\n" +
          "                       # but that you might still want processed. String paths can be absolute\n" +
          "                       # or relative to the watch.sourceDir. Regexes are applied to the entire path.\n";
