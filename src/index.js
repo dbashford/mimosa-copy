bb350e34f2791029e7b94494f461294a5d4b24b5
david.bashford <dbashford@bericotechnologies.com>
2014-04-04 21:37:53 -0400

    don't copy text if text already there

--- src/index.js
+++ src/index.js
@@ -8,28 +8,32 @@ var logger = null
   };
 
 var compile = function ( mimosaConfig, options, next ) {
-  var hasFiles = options.files && options.files.length > 0;
-  if (!hasFiles) {
-    return next();
-  }
 
-  var c = mimosaConfig.copy;
+  if ( options.files && options.files.length ) {
+    var c = mimosaConfig.copy;
 
-  options.files.forEach( function ( file ) {
-    if ( c && c.excludeRegex && file.inputFileName.match( c.excludeRegex ) ) {
-      if ( logger.isDebug() ) {
-        logger.debug( "skipping copy file [[ " + file.inputFileName + " ]], file is excluded via regex" );
-      }
-    } else {
-      if ( c.exclude.indexOf(file.inputFileName) > -1 ) {
+    options.files.forEach( function ( file ) {
+      if ( file.outputFileText ) {
         if ( logger.isDebug() ) {
-          logger.debug( "skipping copy file [[ " + file.inputFileName + " ]], file is excluded via string path" );
+          logger.debug( "skipping copy file [[ " + file.inputFileName + " ]], file aleady has output text." );
         }
       } else {
-        file.outputFileText = file.inputFileText;
+        if ( c && c.excludeRegex && file.inputFileName.match( c.excludeRegex ) ) {
+          if ( logger.isDebug() ) {
+            logger.debug( "skipping copy file [[ " + file.inputFileName + " ]], file is excluded via regex" );
+          }
+        } else {
+          if ( c.exclude.indexOf(file.inputFileName) > -1 ) {
+            if ( logger.isDebug() ) {
+              logger.debug( "skipping copy file [[ " + file.inputFileName + " ]], file is excluded via string path" );
+            }
+          } else {
+            file.outputFileText = file.inputFileText;
+          }
+        }
       }
-    }
-  });
+    });
+  }
 
   next();
 };
